ed56288c9e7b909d83c014a77dd3007b74595e1e
lasse-schmidt <l.s10600@gmail.com>
2022-09-01 08:21:21 +0200

    Styled header with logo and navigator. Added favicon.

--- index.php
+++ index.php
@@ -4,13 +4,34 @@
     <meta charset="utf-8">
     <title>Ønskeportalen</title>
 
+    <link rel="icon" type="image/png" href="gfx/favicon.png">
+
     <!--Reset css-->
     <link rel="stylesheet" type="text/css" href="css/reset.css" />
 
+    <!--Google font Niconne-->
+    <link href="https://fonts.googleapis.com/css2?family=Niconne&display=swap" rel="stylesheet">
+
     <!--Style css-->
     <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+
   </head>
   <body>
+    <!--Header-->
+    <header class="main-header">
+      <div class="logo">
+        <a href="">
+          <img src="gfx/favicon.png" alt="Ønskeportalen logo" />
+          <h1>Ønskeportalen</h1>
+        </a>
+      </div>
 
+      <nav class="main-navigator">
+        <ul>
+          <li><a href="">Login</a></li>
+        </ul>
+      </nav>
+    </header>
   </body>
 </html>
